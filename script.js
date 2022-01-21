95fb50a756245f771b168a01484838607a572cd1
aprilnicole013 <aprilnicole013@gmail.com>
2022-01-21 00:07:07 -0500

    Right click to remove todo

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ form.addEventListener('submit', (e) => {
             todoEl.classList.toggle('completed')
         })
 
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+            
+            todoEl.remove()
+        })
+
         todos.appendChild(todoEl)
 
         input.value = ''
